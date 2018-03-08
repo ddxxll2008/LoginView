37963d76a31c0085d00be67f83b1c501a68c7f01
ddxxll2008 <dangshixiaolei@gmail.com>
2018-03-08 11:34:43 +0800

    modify project structure

--- sample/src/main/java/com/iflytek/loginviewsample/MainActivity.java
+++ sample/src/main/java/com/iflytek/loginviewsample/MainActivity.java
@@ -1,17 +1,13 @@
-package com.iflytek.loginviewtest;
+package com.iflytek.loginviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.iflytek.loginview.LoginView;
-
 public class MainActivity extends AppCompatActivity {
-    private LoginView loginView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginView = findViewById(R.id.loginview);
     }
 }
